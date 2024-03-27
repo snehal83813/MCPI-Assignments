e7c85929a68096f141258538a234423ed4f0ea68
Snehal212Mahajan <mahajansnehal227@gmail.com>
2024-03-27 18:27:30 +0530

    solutions

---
+++ day3_ass1/Inc/ass1.h
@@ -0,0 +1,24 @@
+/*
+ * ass1.h
+ *
+ *  Created on: Mar 27, 2024
+ *      Author: snehal
+ */
+
+#ifndef ASS1_H_
+#define ASS1_H_
+
+#include"stm32f407xx.h"
+#define LED_GPIO  GPIOD
+#define LED_GPIO_EN  3
+#define LED_GREEN  12
+#define LED_ORANGE  13
+#define LED_RED  14
+#define LED_BLUE  15
+
+void LedInit(uint32_t pin);
+void LedOn(uint32_t pin);
+void LedOff(uint32_t pin);
+void LedBlink(uint32_t pin,uint32_t delay);
+
+#endif /* ASS1_H_ */
